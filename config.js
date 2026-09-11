b4c2452922aed7e48a0d5a87d9a82d84ae44dd10
jozemalnaric-eng <joze.malnaric@gmail.com>
2026-09-11 13:20:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 window.APP_CONFIG = {
-  // OAuth Client ID tipa "Web application"
+  // OAuth Client ID tipa "Web application" iz istega Google Cloud projekta kot Apps Script.
   OAUTH_CLIENT_ID: '374777826534-155s4ocl5r0lsggn1ih27nbhd8pkjsja.apps.googleusercontent.com',
 
-  // Deployment ID API executable (NI /exec URL in NI Script ID)
+  // Deployment ID API executable (NI /exec URL in NI Script ID).
   API_DEPLOYMENT_ID: 'AKfycbwEjrAK1MWlfUiAuhxlKkPahb_BYvh7fHkKAk4pTqeAB3GsgykqMKY8sCwERPCtZdt1Gw',
 
-  // OAuth obsegi, ki jih uporablja aplikacija
+  // Obseg, ki ga uporablja trenutni Code.gs.
   OAUTH_SCOPES: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/script.send_mail',
 
-  // Dovoljeni uporabniki
   ALLOWED_USERS: [
     'joze.malnaric@gmail.com',
     's.malnaric@gmail.com',
