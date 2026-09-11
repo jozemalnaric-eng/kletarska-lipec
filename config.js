1715264e17a1f3935580b8c7ec0f7e68c6ebd041
jozemalnaric-eng <joze.malnaric@gmail.com>
2026-09-11 13:10:11 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 window.APP_CONFIG = {
-  // OAuth Client ID tipa "Web application" iz istega Google Cloud projekta kot Apps Script.
-  OAUTH_CLIENT_ID: 'VSTAVI_OAUTH_CLIENT_ID.apps.googleusercontent.com',
+  // OAuth Client ID tipa "Web application"
+  OAUTH_CLIENT_ID: '374777826534-155s4ocl5r0lsggn1ih27nbhd8pkjsja.apps.googleusercontent.com',
 
-  // Deployment ID API executable (NI /exec URL in NI Script ID).
-  API_DEPLOYMENT_ID: 'VSTAVI_API_EXECUTABLE_DEPLOYMENT_ID',
+  // Deployment ID API executable (NI /exec URL in NI Script ID)
+  API_DEPLOYMENT_ID: 'AKfycbwEjrAK1MWlfUiAuhxlKkPahb_BYvh7fHkKAk4pTqeAB3GsgykqMKY8sCwERPCtZdt1Gw',
 
-  // Obseg, ki ga uporablja trenutni Code.gs.
+  // OAuth obsegi, ki jih uporablja aplikacija
   OAUTH_SCOPES: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/script.send_mail',
 
+  // Dovoljeni uporabniki
   ALLOWED_USERS: [
     'joze.malnaric@gmail.com',
     's.malnaric@gmail.com',
